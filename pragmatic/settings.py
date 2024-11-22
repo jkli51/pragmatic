1237478471b555570b5b64523cb26fc9dbdad513
jkli51 <jkk307@naver.com>
2024-11-22 19:45:38 +0900

    django course 34 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -48,9 +48,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accountapp',
     'profileapp',
-    'bootstrap4',
+    'articleapp',
 ]
 
 MIDDLEWARE = [
